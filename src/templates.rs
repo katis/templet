a39573daa94b1585ae1fda938af88bed0415c525
Joni Katajamäki <joni.katajamaki@icloud.com>
2022-03-09 17:40:53 +0200

    Load templates from directory

--- src/templates.rs
+++ src/templates.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, io::Write};
+use std::{collections::HashMap, fs, io::Write, path::Path};
 
+use thiserror::Error;
 use valuable::Valuable;
 
 use crate::{errors::Error, render::Renderer, Template};
@@ -29,6 +30,41 @@ impl Templates {
         renderer.render(name, &mut buf)?;
         Ok(String::from_utf8(buf).unwrap())
     }
+
+    pub fn load_dir(
+        dir_path: impl AsRef<Path>,
+        extensions: &[&str],
+    ) -> Result<Templates, TemplateLoadError> {
+        let dir_path: &Path = dir_path.as_ref();
+
+        let mut templates: HashMap<String, Template> = HashMap::new();
+        for entry in walkdir::WalkDir::new(dir_path) {
+            let entry = entry?;
+            if entry.file_type().is_file()
+                && entry
+                    .path()
+                    .extension()
+                    .map(|ext| extensions.iter().any(|s| ext == *s))
+                    .unwrap_or(false)
+            {
+                let file = fs::read_to_string(entry.path())?;
+                let path = entry.path().strip_prefix(dir_path)?.to_string_lossy();
+                let template = Template::parse(&file);
+                templates.insert(path.into_owned(), template);
+            }
+        }
+        Ok(Templates::new(templates))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum TemplateLoadError {
+    #[error("failed to load template files")]
+    Io(#[from] std::io::Error),
+    #[error("could not strip path prefix")]
+    StripPrefix(#[from] std::path::StripPrefixError),
+    #[error("could not walk template directory")]
+    WalkDir(#[from] walkdir::Error),
 }
 
 #[cfg(test)]
@@ -40,6 +76,7 @@ mod tests {
     #[derive(Valuable)]
     struct Page<'a> {
         title: &'a str,
+        css: &'a [&'a str],
         items: &'a [Item<'a>],
     }
 
@@ -67,6 +104,7 @@ mod tests {
                 "main",
                 &Page {
                     title: "Products",
+                    css: &[],
                     items: &[Item { name: "Bread" }, Item { name: "Milk" }],
                 },
             )
@@ -75,5 +113,52 @@ mod tests {
             &str,
             "<h1>Products</h1><ul><li>Bread</li><li>Milk</li></ul>"
         );
+    }
+
+    #[test]
+    fn load_dir() {
+        let templates = Templates::load_dir("./templates", &["html"]).unwrap();
+        let str = templates
+            .render_to_string(
+                "index.html",
+                &Page {
+                    title: "Products",
+                    css: &["/index.css", "/main.css"],
+                    items: &[Item { name: "Bread" }, Item { name: "Milk" }],
+                },
+            )
+            .unwrap();
+        println!("{}", &str);
+
+        let expected = r#"
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  <link rel="stylesheet" href="&#x2f;index.css" />
+  
+  <link rel="stylesheet" href="&#x2f;main.css" />
+  
+  <title>Products</title>
+</head>
+
+  <body>
+    <h1>Products</h1>
+    <ul>
+      
+      <li>Bread</li>
+      
+      <li>Milk</li>
+      
+    </ul>
+    <div class="footer">
+  <a href="/">Home</a>
+</div>
+
+  </body>
+</html>
+"#
+        .trim_start();
+        assert_eq!(&str, expected);
     }
 }
