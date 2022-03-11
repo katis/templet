8dc77ba1745e49b3ebe0daf1743237ced8838c50
Joni Katajamäki <joni.katajamaki@icloud.com>
2022-03-11 08:27:10 +0200

    Add handlebars comparison benchmark

--- benches/template.rs
+++ benches/template.rs
@@ -1,34 +1,36 @@
 use std::collections::HashMap;
 
 use criterion::{criterion_group, criterion_main, Criterion};
+use handlebars::Handlebars;
 use ramhorns::Content;
+use serde::Serialize;
 use templet::{Template, Templates};
 use valuable::Valuable;
 
-#[derive(Valuable, Content)]
+#[derive(Valuable, Content, Serialize)]
 struct Page<'a> {
     title: &'a str,
     products: &'a [Product<'a>],
 }
 
-#[derive(Valuable, Content)]
+#[derive(Valuable, Content, Serialize)]
 struct Item<'a> {
     name: &'a str,
 }
 
-#[derive(Valuable, Content)]
+#[derive(Valuable, Content, Serialize)]
 struct Product<'a> {
     name: &'a str,
     price: Price,
     images: &'a [Image<'a>],
 }
 
-#[derive(Valuable, Content)]
+#[derive(Valuable, Content, Serialize)]
 struct Price {
     price: i32,
 }
 
-#[derive(Valuable, Content)]
+#[derive(Valuable, Content, Serialize)]
 struct Image<'a> {
     title: &'a str,
     href: &'a str,
@@ -61,6 +63,32 @@ pub fn criterion_benchmark(c: &mut Criterion) {
       </body>
     </html>"#;
 
+    static PAGE_HANDLEBARS: &str = r#"
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>{{title}}<title>
+      </head>
+      <body>
+        <h1>{{title}}</h1>
+        <ul>
+          {{#each product}}
+            <li>
+              <h2>{{name}}</h2>
+              {{#with price}}
+                Price: {{price}}e
+              {{/with}}
+              <ul>
+                {{#each images}}
+                  <li><img alt="{{title}}" href="{{href}}"></li>
+                {{/each}}
+              </ul>
+            </li>
+          {{/each}}
+        </ul>
+      </body>
+    </html>"#;
+
     c.bench_function("parse", |b| {
         let mut template = None;
 
@@ -74,6 +102,14 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             template = ramhorns::Template::new(PAGE).ok();
+        });
+    });
+
+    c.bench_function("parse_handlebars", |b| {
+        let mut template = None;
+
+        b.iter(|| {
+            template = Some(handlebars::Template::compile(PAGE_HANDLEBARS).unwrap());
         });
     });
 
@@ -155,6 +191,49 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             template.render_to_writer(&mut buf, ctx).unwrap();
+            buf.clear();
+        });
+    });
+
+    c.bench_function("render_handlebars", |b| {
+        let mut buf = Vec::new();
+
+        let template = handlebars::Template::compile(PAGE_HANDLEBARS).unwrap();
+        let mut reg = Handlebars::new();
+        reg.register_template("template", template);
+
+        let ctx = &Page {
+            title: "Weird store",
+            products: &[
+                Product {
+                    name: "Netflix subscription",
+                    images: &[Image {
+                        title: "Netflix",
+                        href: "/netflix.logo.svg",
+                    }],
+                    price: Price { price: 13 },
+                },
+                Product {
+                    name: "Artisan Bread",
+                    images: &[Image {
+                        title: "Bread",
+                        href: "/bread.jpg",
+                    }],
+                    price: Price { price: 4 },
+                },
+                Product {
+                    name: "Orange juice",
+                    images: &[Image {
+                        title: "Orange juice",
+                        href: "/orange-juice.jpg",
+                    }],
+                    price: Price { price: 4 },
+                },
+            ],
+        };
+
+        b.iter(|| {
+            reg.render_to_write("template", ctx, &mut buf).unwrap();
             buf.clear();
         });
     });
